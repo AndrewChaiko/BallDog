a81e4badcbcd3935c656339b6a72012dc8a75ad4
Andrew Chaiko <andrewc@playtika.com>
2017-12-28 17:54:19 +0300

    Compiiling MPU6050 with minimum functionality using EyeToSee library

--- main.cpp
+++ main.cpp
@@ -2,15 +2,17 @@
 #include <util/delay.h>
 #include "utils.h"
 #include "EyeToSee/EyeToSee.h"
-
+#include "MPU6050/MPU6050.h"
 int main(void)
 {
 	EyeToSee bus;
+	MPU6050 mpu;
 	unsigned char c;
 	bool success = bus.read(0x68, 0x02, &c);
 	while(success)
 	{
 		_delay_ms(500);
+		mpu.getAccelerationX();
 	}
 
 	return -1;
